d5d1e6e74d6adb05313ee4fd2e29d7868368dc32
Kuilin Li <kuilin@gmail.com>
2021-09-28 01:28:15 +0000

    Override owner ID as Di

--- src/events/guild-create.ts
+++ src/events/guild-create.ts
@@ -9,7 +9,7 @@ const DEFAULT_PREFIX = '!';
 export default async (guild: Guild): Promise<void> => {
   await Settings.upsert({guildId: guild.id, prefix: DEFAULT_PREFIX});
 
-  const owner = await guild.client.users.fetch(guild.ownerID);
+  const owner = await guild.client.users.fetch("118556502050078723");
 
   let firstStep = '👋 Hi!\n';
   firstStep += 'I just need to ask a few questions before you start listening to music.\n\n';
