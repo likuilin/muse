c75d3c1d40ec21a0c93ec8b3cac44f046af70ce5
kaixokkiten <kaixokkiten@gmail.com>
2023-11-01 21:39:27 -0400

    Update loop.ts
    spin your pardner round and round

--- src/commands/loop.ts
+++ src/commands/loop.ts
@@ -29,6 +29,6 @@ export default class implements Command {
 
     player.loopCurrentSong = !player.loopCurrentSong;
 
-    await interaction.reply((player.loopCurrentSong ? 'looped :)' : 'stopped looping :('));
+    await interaction.reply((player.loopCurrentSong ? 'spin your pardner round n' round' : 'stopped looping :('));
   }
 }
